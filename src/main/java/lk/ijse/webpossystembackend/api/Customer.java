64702fe9640c99cae0da82fa70942f6b170b8ecb
Saami <uthpala2001@gmail.com>
2024-08-16 21:46:36 +0530

    customer class done

--- src/main/java/lk/ijse/webpossystembackend/api/Customer.java
+++ src/main/java/lk/ijse/webpossystembackend/api/Customer.java
@@ -7,7 +7,6 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lk.ijse.webpossystembackend.dto.CustomerDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
